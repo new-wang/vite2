be5f17b00797260b017ccd2383c4c6fd6bc6192d
new-wang <1569566735@qq.com>
2022-01-17 15:52:40 +0800

    *：配置别名

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+import HelloWorld from 'comps/HelloWorld.vue'
 </script>
 
 <template>
+  "vite": "^2.7.2"
+
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve:{
+    alias: { 
+      '@': path.resolve(__dirname, 'src'),
+      'comps': path.resolve(__dirname, 'src/components'),
+      'apis': path.resolve(__dirname, 'src/apis'),
+      'views': path.resolve(__dirname, 'src/views'),
+      'utils': path.resolve(__dirname, 'src/utils'),
+      'routes': path.resolve(__dirname, 'src/routes'),
+      'styles': path.resolve(__dirname, 'src/styles'),
+
+    }
+  }
 })
