cc385eb3614a43f0f89cddcde5a7cdf605ea3a50
new-wang <1569566735@qq.com>
2022-01-18 15:04:57 +0800

    *：jsx結合setup；

--- src/components/Comp.vue
+++ src/components/Comp.vue
@@ -1,10 +1,43 @@
 <script lang="jsx">
-    export default {
-        render() {
-            return <div> jsx  comp</div>
+import { ref } from "vue" 
+export default{
+    setup(){
+        const counter = ref(0)
+        const onclick = ()=>{
+            counter.value++
         }
 
+        return () => (
+            <>
+            <div> Comp </div>
+            <p onClick={onclick}>{counter.value}</p>
+            </>
+        )
+
     }
+}
+
+// 原始jsx
+    // export default {
+    //     data() {
+    //         return {
+    //             counter:0
+    //         }
+    //     },
+    //     render() {
+    //         return (
+    //             <>
+    //                 <div>jsx  comp</div>
+    //                 <p onclick={this.onclick}>{this.counter}</p>
+    //             </>
+    //         )
+    //     },
+    //     methods: {
+    //         onclick() {
+    //             this.counter++
+    //         }
+    //     },
+    // }
 </script>
 
 <style scoped>
